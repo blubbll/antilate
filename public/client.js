0d9c7402ba9b20a7cbe5c7640cf2edbac2362d17
Glitch (antilate) <none>
2019-12-11 11:51:14 +0000

    🌴🌵 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,22 +1,62 @@
 //fix da pres
 
+const beauty = str => {
+  return window.html_beautify(str, {
+    indent_size: 2,
+    space_in_empty_paren: false
+  });
+};
+
 fetch("https://antilate.glitch.me/")
   .then(r => r.text())
   .then(t => {
     const original = $($.parseHTML(t));
 
-    $(original.find("section")).each((i, el) => {
-      const curId = $(el)[0].id;
-      console.log(curId);
-      curId && console.log(original.find("#" + curId));
+    if (location.href.startsWith("https://translate.google")) {
+      $(original.find("section")).each((i, el) => {
+        const curId = $(el).prop("id");
 
-      const pre = $(el).find("[pre]");
-      const pos = $(el).find("[pos]");
+        if (curId) {
+          const pre = $(el).find("[pre]");
+          const pos = $(el).find("[pos]");
 
-      const originalHtml = pre.html();
+          const origHtml = $(el)
+            .find("pre[pre]")
+            .html();
+          const brokenHtml = $(`#${curId}`)
+            .find("pre[pos]")
+            .html();
 
-      let newHtml = originalHtml;
-      console.log(newHtml);
-      // $(el).find(text(newHtml));
-    });
+          const brokenHtmlmod = beauty(
+            brokenHtml ? brokenHtml.split(/</).join("\r\n<") : origHtml
+          );
+          const origHtmlmod = beauty(
+            beauty(origHtml)
+              .split(/</)
+              .join("\r\n<")
+          );
+          //fix pre code
+          $(`#${curId}`)
+            .find("pre[pre]")
+            .text(origHtmlmod);
+          //show pos code
+          $(`#${curId}`)
+            .find("pre[pos]")
+            .text(brokenHtmlmod);
+        }
+      });
+    } else {
+      $("pre[pre]").each((i, el) => {
+        $(el).text(
+          beauty(
+            $(el)
+              .html()
+              .split(/</)
+              .join("\r\n<")
+          )
+        );
+      });
+
+      $("pre[pos]").text("not translating 🙉");
+    }
   });
