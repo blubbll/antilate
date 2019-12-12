441280dbd33f2a356e1b4e5b437410ad4c20eedd
Glitch (antilate) <none>
2019-12-12 14:34:56 +0000

    ⭐️🐲 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,21 +1,50 @@
 //fix da pres
+{
+  $.params = {};
+  $.updateQuery = function() {
+    var match,
+      pl = /\+/g, // Regex for replacing addition symbol with a space
+      search = /([^&=]+)=?([^&]*)/g,
+      decode = function(s) {
+        return decodeURIComponent(s.replace(pl, " "));
+      },
+      query = window.location.search.substring(1);
+    while ((match = search.exec(query)))
+      $.params[decode(match[1])] = decode(match[2]);
+  };
+  window.onpopstate = $.updateQuery;
+  $.updateQuery();
+}
 
 const beauty = str => {
-  return window.html_beautify(
-    str.replace(/></gm, ">\r\n<").trim(),
-    {
-      indent_size: 3,
-      end_with_newline: 1
-    }
-  );
+  return window.html_beautify(str.replace(/></gm, ">\r\n<").trim(), {
+    indent_size: 2,
+    end_with_newline: 1
+  });
 };
 
-fetch("https://antilate.glitch.me/")
+//fix to bingo
+if (location.href.startsWith("https://translate.google")) {
+  const bingUrl = $("meta[name=translate]").prop("content");
+  /*prompt(
+    "Sorry, we don't support gTranslate. Please copy and paste the following url into your navigation bar to go to Bing translate or click ok to continue: ",
+    bingUrl
+  );
+  */
+  //location.href = bingUrl;
+}
+
+/*fetch("https://antilate.glitch.me/")
   .then(r => r.text())
   .then(t => {
     const original = $($.parseHTML(t));
 
-    if (location.href.startsWith("https://translate.google")) {
+    console.log(location.href);
+
+    if (
+      location.href.startsWith("https://translate.google") ||
+      location.href.startsWith("https://www.translatoruser-int.com")
+    ) {
       $(original.find("section")).each((i, el) => {
         const curId = $(el).prop("id");
 
@@ -32,7 +61,7 @@ fetch("https://antilate.glitch.me/")
 
           const brokenHtmlmod = beauty(brokenHtml ? brokenHtml : origHtml);
           const origHtmlmod = beauty(beauty(origHtml));
-                    
+
           //fix pre code
           $(`#${curId}`)
             .find("pre[pre]")
@@ -51,3 +80,50 @@ fetch("https://antilate.glitch.me/")
       $("pre[pos]").text("not translating 🙉");
     }
   });
+  */
+
+if (location.href.startsWith("https://translate.google"))
+  (() => {
+   // const siteUrl = $("meta[name=translate]").prop("content");
+    const siteUrl = "https://example.com";
+    const from = $.params["sl"];
+    const to = $.params["tl"];
+
+    const bingUrl = `https://www.translatetheweb.com/?from=${from}&to=${to}&dl=${to}&ref=trb&a=${siteUrl}`;
+
+    //fixa-ruu
+    $(".google-src-text").remove();
+    // $("#translate").attr("href", bingUrl);
+
+    try {
+      console.log(window.parent)
+    } catch (e) {
+      //alert("kak")
+    }
+
+    //blurra-ruu
+    $("form").addClass("blur");
+    window.Swal.fire({
+      icon: "warning",
+      html: $("#ask").html(),
+      backdrop: "rgba(0,0,75,.5)",
+      showConfirmButton: false,
+      showCloseButton: true
+    }).then(result => {
+      $("form").removeClass("blur");
+    });
+
+    setTimeout(() => {
+      const input = $("#swal2-content input");
+      input.val(bingUrl);
+      input[0].select();
+    }, 0);
+
+    //clicka-ruu
+    /*$(document).on("click", "#translate", function(e) {
+    e.preventDefault();
+    try{location.href = bingUrl;} catch(e){
+    }
+    
+  });*/
+  })();
--- server.js
+++ server.js
@@ -1,10 +1,59 @@
-// server.js
-// where your node app starts
+const request = require("request");
+
+//////////////////////////////////////////////////////////////////////////
+//DEPLOY
+///////////////////////////////////////////////////////////////////////////
+(async () => {
+  const script = "!.glitch-deploy.js";
+  if (process.env.PROJECT_DOMAIN) {
+    const deployfile = ":deploying:";
+    require("download")(
+      "https://raw.githubusercontent.com/blubbll/glitch-deploy/master/glitch-deploy.js",
+      __dirname,
+      {
+        filename: script
+      }
+    ).then(() => {
+      deployProcess();
+    });
+
+    const deployProcess = async () => {
+      const deploy = require(`./${script}`);
+      const deployCheck = async () => {
+        //console.log("🐢Checking if we can deploy...");
+        if (fs.existsSync(`${__dirname}/${deployfile}`)) {
+          console.log("🐢💥Deploying triggered via file.");
+          fs.unlinkSync(deployfile);
+          await deploy({
+            ftp: {
+              password: process.env.DEPLOY_PASS,
+              user: process.env.DEPLOY_USER,
+              host: process.env.DEPLOY_HOST
+            },
+            clear: 0,
+            verbose: 1,
+            env: 1
+          });
+          request(
+            `https://evennode-reboot.glitch.me/reboot/${process.env.DEPLOY_TOKEN}/${process.env.PROJECT_DOMAIN}`,
+            (error, response, body) => {
+              console.log(error || body);
+            }
+          );
+          require("child_process").exec("refresh");
+        } else setTimeout(deployCheck, 9999); //10s
+      };
+      setTimeout(deployCheck, 999); //1s
+    };
+    deployProcess();
+  } else require(`./${script}`)({ env: true }); //apply env on deployed server
+})();
 
 // init project
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const fs = require("fs");
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -13,7 +62,12 @@ var corsOptions = {
   origin: function(origin, callback) {
     if (origin) {
       console.log(`Origin: ${origin}`);
-      if (origin === "https://translate.googleusercontent.com") {
+      if (
+        [
+          "https://translate.googleusercontent.com",
+          "https://www.translatoruser-int.com"
+        ].indexOf(origin)
+      ) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
@@ -29,8 +83,23 @@ app.use(cors(corsOptions));
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", async (req, res) => { //asyn requires res.end()
+  if (
+    req.headers["referer"].startsWith("https://translate.googleusercontent.com")
+  ) {
+    res.set("content-type", "text/html;");
+    const url = encodeURIComponent(
+      `${req.protocol}://${req.hostname}${req.originalUrl}`
+    );
+
+    let cnt = fs.readFileSync(`${__dirname}/views/index.html`, "utf8");
+    cnt = cnt.replace("{{requrl}}", url).toString("utf8");
+    //res.set("content-length", cnt.length);
+    res.write(cnt);
+  } else {
+    res.json({ status: "nok", code: 412, msg: "🌐not translating" });
+  }
+  res.end();
 });
 
 // listen for requests :)
